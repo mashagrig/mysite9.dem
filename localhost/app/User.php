a7b12e51296af87ba25b8071a41e13f741889490
mashagrig <m-a-grigoreva@yandex.ru>
2019-03-27 12:07:17 +0300

    models: Personal_info, Role, User

--- localhost/app/User.php
+++ localhost/app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -15,10 +15,11 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = "users";
     protected $fillable = [
-        'name', 'email', 'password',
+        'id', 'name', 'email', 'password', 'user_personal_info_id', 'user_role_id'
     ];
-
+    //protected $guarded = [];
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -36,4 +37,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function personal_infos(){
+        return $this->belongsTo("App\Personal_info", "user_personal_info_id", "id");
+    }
+
+    public function role(){
+       return $this->belongsTo("App\Role", "user_role_id", "id");
+    }
+
 }
