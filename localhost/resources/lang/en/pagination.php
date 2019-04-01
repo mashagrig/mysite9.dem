5ab0cec4df76b67494e65544bb35a21b74b429f8
mashagrig <m-a-grigoreva@yandex.ru>
2019-04-01 10:35:25 +0300

    paginator

--- localhost/resources/lang/en/pagination.php
+++ localhost/resources/lang/en/pagination.php
@@ -13,7 +13,10 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next' => 'Next &raquo;',
+    'previous' => '&laquo; Назад',
+    'next' => 'Вперед &raquo;',
+
+//    'previous' => '&laquo; Previous',
+//    'next' => 'Next &raquo;',
 
 ];
