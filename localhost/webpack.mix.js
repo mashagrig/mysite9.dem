05df5e6aaff6bab2a83a7caceead346c0822f494
mashagrig <m-a-grigoreva@yandex.ru>
2019-04-05 22:00:15 +0300

    perfect!!! installed all tempate's dependenses-1

--- localhost/webpack.mix.js
+++ localhost/webpack.mix.js
@@ -23,36 +23,67 @@ const mix = require('laravel-mix');
 //         })
 //     ]
 // });
+
+// mix
+//     .copy([
+//         'resources/crossfits/fonts/all/Flaticon.eot',
+//         'resources/crossfits/fonts/all/flaticon.html',
+//         'resources/crossfits/fonts/all/Flaticon.svg',
+//         'resources/crossfits/fonts/all/Flaticon.ttf',
+//         'resources/crossfits/fonts/all/Flaticon.woff',
+//         'resources/crossfits/fonts/all/icomoon.eot',
+//         'resources/crossfits/fonts/all/icomoon.svg',
+//         'resources/crossfits/fonts/all/icomoon.ttf',
+//         'resources/crossfits/fonts/all/icomoon.woff',
+//         'resources/crossfits/fonts/all/selection.json'
+//
+//     ],  'public/css/fonts')
+//
+//     .copy([
+//         'resources/crossfits/images'
+//     ],  'public/images');
+//
+// mix.copy([
+//     'node_modules/popper.js/dist/popper.js',
+//     'node_modules/popper.js/dist/popper.js.map',
+//     'node_modules/popper.js/dist/popper.min.js.map',
+//     'node_modules/jquery/dist/jquery.js',
+//     'node_modules/jquery/dist/jquery.min.map',
+//     'node_modules/jquery-migrate/dist/jquery-migrate.min.js',
+//     'node_modules/jquery-ui-css/jquery-ui.min.js',
+//     'node_modules/bootstrap/dist/js/bootstrap.min.js',
+//     'node_modules/bootstrap/dist/js/bootstrap.js.map',
+//     'node_modules/bootstrap/dist/js/bootstrap.min.js.map',
+//     'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+//     'node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
+//     'node_modules/owl.carousel/dist/owl.carousel.min.js',
+//     'node_modules/jquery.stellar/jquery.stellar.js',
+//     'node_modules/jquery.countdown/jquery.countdown.js',
+//     'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+//     'node_modules/purify/lib/purify.js',
+//     'node_modules/aos/dist/aos.js'
+//
+// ], 'public/js/libs');
+
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css/app.css');
+
 
 mix
     .styles(
     [
-       // 'resources/crossfits/css/style.css',
-
-        'node_modules/jquery-ui/themes/base/base.css',
-
+        'resources/crossfits/fonts/all/flaticon.css',
+        'resources/crossfits/fonts/all/style.css',
+         'node_modules/jquery-ui-css/jquery-ui.min.css',
         'node_modules/bootstrap/dist/css/bootstrap.css',
-        'node_modules/bootstrap/dist/css/bootstrap-grid.css',
-        'node_modules/bootstrap/dist/css/bootstrap-reboot.css',
-
-        'node_modules/aos/dist/aos.css',
-
         'node_modules/magnific-popup/dist/magnific-popup.css',
-
-        'node_modules/mediaelement/src/css/mediaelementplayer.css',
-        'node_modules/mediaelement/src/css/mediaelementplayer-legacy.css',
-
-        'node_modules/owl.carousel/dist/assets/owl.carousel.css',
-        'node_modules/owl.carousel/dist/assets/owl.theme.default.css',
-        'node_modules/owl.carousel/dist/assets/owl.theme.green.css',
-
+        'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css',
+        'node_modules/mediaelement/build/mediaelementplayer.min.css',
+        'node_modules/owl.carousel/dist/assets/owl.carousel.min.css',
+        'node_modules/owl.carousel/dist/assets/owl.theme.default.min.css',
         'node_modules/animate.css/animate.min.css',
-
-        'resources/crossfits/fonts/flaticon/font/flaticon.css',
-        'resources/crossfits/fonts/icomoon/style.css',
-
+        'node_modules/aos/dist/aos.css',
         //'resources/crossfits/css/style.css',
         //'resources/sass/laravel.css'
     ],
@@ -60,20 +91,10 @@ mix
 )
     .styles(
     [
-        'resources/crossfits/css/style.css'
+        'public/css/draft/style.css'
     ],
     'public/css/style.css'
 );
 
 
-
-mix
-    .copy([
-    'resources/crossfits/fonts/flaticon/font',
-    'resources/crossfits/fonts/icomoon/fonts'
-],  'public/fonts')
-
-    .copy([
-    'resources/crossfits/images'
-],  'public/images');
 
