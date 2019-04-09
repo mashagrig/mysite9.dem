ed84846b75183d4f63bfbd0036ecbd7282f19bc5
mashagrig <m-a-grigoreva@yandex.ru>
2019-04-09 19:37:28 +0300

    add contacts yandex map

--- localhost/resources/views/contacts/contacts.blade.php
+++ localhost/resources/views/contacts/contacts.blade.php
@@ -1,67 +1,116 @@
+
+{{--<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Afe6f29db80750e3157771caa6fe94945176db7d8bb70265a743b0c973feede3d&amp;source=constructor" width="100%" height="443" frameborder="0"></iframe>--}}
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afe6f29db80750e3157771caa6fe94945176db7d8bb70265a743b0c973feede3d&amp;width=100%25&amp;height=443&amp;lang=ru_RU&amp;scroll=false"></script>
+
 <div class="py-5 bg-light">
     <div class="container">
         <div class="row">
 
-            <div class="col-md-12 col-lg-8 mb-5">
-
-
-
-                <form action="#" class="p-5 bg-white">
-
-                    <div class="row form-group">
-                        <div class="col-md-12 mb-3 mb-md-0">
-                            <label class="font-weight-bold" for="fullname">Full Name</label>
-                            <input type="text" id="fullname" class="form-control" placeholder="Full Name">
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                        <div class="col-md-12">
-                            <label class="font-weight-bold" for="email">Email</label>
-                            <input type="email" id="email" class="form-control" placeholder="Email Address">
-                        </div>
-                    </div>
-
-
-                    <div class="row form-group">
-                        <div class="col-md-12 mb-3 mb-md-0">
-                            <label class="font-weight-bold" for="phone">Phone</label>
-                            <input type="text" id="phone" class="form-control" placeholder="Phone #">
-                        </div>
-                    </div>
-
-                    <div class="row form-group">
-                        <div class="col-md-12">
-                            <label class="font-weight-bold" for="message">Message</label>
-                            <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Say hello to us"></textarea>
-                        </div>
-                    </div>
-
-                    <div class="row form-group">
-                        <div class="col-md-12">
-                            <input type="submit" value="Send Message" class="btn btn-primary text-white px-4 py-2">
-                        </div>
-                    </div>
-
-
-                </form>
-            </div>
-
             <div class="col-lg-4">
                 <div class="p-4 mb-3 bg-white">
-                    <h3 class="h5 text-black mb-3">Contact Info</h3>
-                    <p class="mb-0 font-weight-bold">Address</p>
-                    <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
+                    <h3 class="h5 text-black mb-3">Контакная информация</h3>
+                    <p class="mb-0 font-weight-bold">Адрес клуба</p>
+                    <p class="mb-4">г.Москва, ул. Правды, д.1</p>
 
-                    <p class="mb-0 font-weight-bold">Phone</p>
-                    <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
+                    <p class="mb-0 font-weight-bold">Телефон</p>
+                    <p class="mb-4"><a class="top-register" href="+7-999-876-54-32">+7-999-876-54-32</a></p>
 
-                    <p class="mb-0 font-weight-bold">Email Address</p>
-                    <p class="mb-0"><a href="#">youremail@domain.com</a></p>
+                    <p class="mb-0 font-weight-bold">Email-адрес</p>
+                    <p class="mb-0"><a class="top-register" href="#">support@sport.ru</a></p>
 
                 </div>
 
 
             </div>
+            <div class="col-lg-8">
+                <div class="p-4 bg-white  mb-3">
+                    <h3 class="h5 text-black mb-3">Напишите нам</h3>
+                    {{--<div class="col-md-12 col-lg-8 mb-5  bg-white">--}}
+                    <form action="#" class="mb-0 bg-white">
+
+                        <div class="row form-group">
+                            <div class="col-md-12 mb-3 mb-md-0">
+                                <label class="font-weight-bold" for="fullname">Ваше имя</label>
+                                <input type="text" id="fullname" class="form-control" placeholder="Имя">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label class="font-weight-bold" for="email">Email</label>
+                                <input type="email" id="email" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+
+
+                        <div class="row form-group">
+                            <div class="col-md-12 mb-3 mb-md-0">
+                                <label class="font-weight-bold" for="phone">Телефон</label>
+                                <input type="text" id="phone" class="form-control" placeholder="Телефон">
+                            </div>
+                        </div>
+
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label class="font-weight-bold" for="message">Сообщение</label>
+                                <textarea name="message" id="message" cols="30" rows="3" class="form-control" placeholder="Сообщение..."></textarea>
+                            </div>
+                        </div>
+
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <input type="submit" value="Отправить" class="btn btn-primary text-white px-4 py-2">
+                            </div>
+                        </div>
+
+
+                    </form>
+
+
+
+                </div>
+            </div>
         </div>
+
+
+        {{--<form action="#" class="p-5 bg-white">--}}
+            {{--<h3 class="h5 text-black mb-3">Напишите нам</h3>--}}
+            {{--<div class="row form-group">--}}
+                {{--<div class="col-md-12 mb-3 mb-md-0">--}}
+                    {{--<label class="font-weight-bold" for="fullname">Ваше имя</label>--}}
+                    {{--<input type="text" id="fullname" class="form-control" placeholder="Имя">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="row form-group">--}}
+                {{--<div class="col-md-12">--}}
+                    {{--<label class="font-weight-bold" for="email">Email</label>--}}
+                    {{--<input type="email" id="email" class="form-control" placeholder="Email">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+
+            {{--<div class="row form-group">--}}
+                {{--<div class="col-md-12 mb-3 mb-md-0">--}}
+                    {{--<label class="font-weight-bold" for="phone">Телефон</label>--}}
+                    {{--<input type="text" id="phone" class="form-control" placeholder="Телефон">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+            {{--<div class="row form-group">--}}
+                {{--<div class="col-md-12">--}}
+                    {{--<label class="font-weight-bold" for="message">Сообщение</label>--}}
+                    {{--<textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Сообщение..."></textarea>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+            {{--<div class="row form-group">--}}
+                {{--<div class="col-md-12">--}}
+                    {{--<input type="submit" value="Отправить" class="btn btn-primary text-white px-4 py-2">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+
+        {{--</form>--}}
     </div>
 </div>
+
+
