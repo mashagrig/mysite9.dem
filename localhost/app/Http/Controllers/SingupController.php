f934992410b2b9b45b31eff97ce149ffce6e6fe1
mashagrig <m-a-grigoreva@yandex.ru>
2019-04-17 23:25:29 +0300

    insert training for user

--- localhost/app/Http/Controllers/SingupController.php
+++ localhost/app/Http/Controllers/SingupController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Guest;
+use App\Singup;
 use App\Trainingshedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SingupController extends Controller
 {
@@ -14,13 +17,13 @@ class SingupController extends Controller
      */
     public function index()
     {
-        $check_shedule_id = '';
-        $max_date_select = '';
+        $check_shedule_id = array();
+        $max_date_select = array();
         $each_check_shedule_info = array();
 
 
         return view('privacy', [
-            'check_shedule_id'=>$check_shedule_id,
+            'check_shedule_id' => $check_shedule_id,
             'max_date_select' => $max_date_select,
             'each_check_shedule_info' => $each_check_shedule_info,
         ]);
@@ -39,88 +42,111 @@ class SingupController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $check_shedule_id = '';
-        $max_date_select = '';
+        $check_shedule_id = array();
+        $max_date_select = array();
         $each_check_shedule_info = array();
 
+        $current_user = Auth::user()->getAuthIdentifier();
 
-        if(isset($request->check_shedule_id) && (!empty($request->check_shedule_id)))
-        {
+        $current_guest = Guest::select('guests.id')
+            ->join('users', function ($join) {
+                $join->on('users.id', '=', 'guests.user_id');
+            })
+            ->where('users.id', '=', "{$current_user}")
+            ->get('id')[0]->id;
+
+
+
+        if (isset($request->check_shedule_id) && (!empty($request->check_shedule_id))) {
             $check_shedule_id = $request->check_shedule_id;
 
-            foreach ($check_shedule_id as $k=>$id){
+            foreach ($check_shedule_id as $k => $id) {
 
-                array_push ( $each_check_shedule_info ,
-                 Trainingshedule::select(
-                    'trainingshedules.id as shedule_id',
-                    'trainingshedules.date_training as date_training',
-                    'trainingtimes.start_training as start_training',
-                    'trainingtimes.stop_training as stop_training',
-                    'trainingshedules.user_id as trainer_id',
-                    'personalinfos.name as trainer_name',
-                    'trainingshedules.section_id as section_id',
-                    'sections.title as section_title',
-                    'trainingshedules.gym_id as gym_id'
-                )
-                    ->join('users', function ($join) {
-                        $join->on('users.id', '=', 'trainingshedules.user_id');
-                    })
-                    ->join('personalinfos', function ($join) {
-                        $join->on('personalinfos.id', '=', 'users.personalinfo_id');
-                    })
-                    ->join('roles', function ($join) {
-                        $join->on('roles.id', '=', 'users.role_id');
-                    })
-                    ->join('trainingtimes', function ($join) {
-                        $join->on('trainingtimes.id', '=', 'trainingshedules.trainingtime_id');
-                    })
-
-                    ->join('sections', function ($join) {
-                        $join->on("sections.id", '=', 'trainingshedules.section_id');
-                    })
-                    ->join('gyms', function ($join) {
-                        $join->on('gyms.id', '=', 'trainingshedules.gym_id');
-                    })
-
-
-                    ->where('trainingshedules.id', '=', "{$id}")
-
-                    ->oldest('date_training')
-                    ->oldest('start_training')
-                    ->get()
-                    );
-
-//                $max_date_select = $each_check_shedule_info
-//                    ->unique('date_training')
-//                    ->pluck('date_training')
-//                    ->toArray();
+                //запись в базу запрошенных тренировок на пользователя
+                $insert_in_singups = Singup::create([
+                    'guest_id' => $current_guest,
+                    'trainingshedule_id' => $id
+                ]);
 
 
 
-//            foreach ($check_shedule_id as $k=>$id){
-//
-////                $each_check_shedule_info = $info
-////                    ->where('trainingshedules.id', '=', "{$id}");
-//
-//                   // ->where('trainingshedules.id', '=', "{$id}")
-//                   // ->unique('shedule_id')
-//                  //  ->pluck('id')
-//                   // ->toArray();
 
 
+                //вывод запрашиваемых запией на тренировки
+                //вывод сохраненных??? запией на тренировки
+                array_push($each_check_shedule_info,
+                    Trainingshedule::select(
+                        'trainingshedules.id as shedule_id',
+                        'trainingshedules.date_training as date_training',
+                        'trainingtimes.start_training as start_training',
+                        'trainingtimes.stop_training as stop_training',
+                        'trainingshedules.user_id as trainer_id',
+                        'personalinfos.name as trainer_name',
+                        'trainingshedules.section_id as section_id',
+                        'sections.title as section_title',
+                        'trainingshedules.gym_id as gym_id'//,
+                     //   'singups.guest_id as guest_id',
+                     //   'singups.id as singup_id'
+                    )
+                        ->join('users', function ($join) {
+                            $join->on('users.id', '=', 'trainingshedules.user_id');
+                        })
+                        ->join('personalinfos', function ($join) {
+                            $join->on('personalinfos.id', '=', 'users.personalinfo_id');
+                        })
+                        ->join('roles', function ($join) {
+                            $join->on('roles.id', '=', 'users.role_id');
+                        })
+//                        ->join('guests', function ($join) {
+//                            $join->on('guests.user_id', '=', 'users.id');
+//                        })
+
+                        ->join('trainingtimes', function ($join) {
+                            $join->on('trainingtimes.id', '=', 'trainingshedules.trainingtime_id');
+                        })
+                        ->join('sections', function ($join) {
+                            $join->on("sections.id", '=', 'trainingshedules.section_id');
+                        })
+                        ->join('gyms', function ($join) {
+                            $join->on('gyms.id', '=', 'trainingshedules.gym_id');
+                        })
+//                        ->join('singups', function ($join) {
+//                            $join->on('singups.trainingshedule_id', '=', 'trainingshedules.id');
+//                        })
+
+                        ->where('trainingshedules.id', '=', "{$id}")
+                     //   ->where('singups.guest_id', '=', "{$current_user}")
+                     //   ->where('users.id', '=', "{$current_user}")
+                        ->oldest('date_training')
+                        ->oldest('start_training')
+                        ->get()
+                );
 
             }
         }
+                foreach ($each_check_shedule_info as $kk => $singup) {
 
+                    foreach ($singup as $k => $v) {
+
+                        array_push(
+                            $max_date_select,
+                            $singup[$k]['date_training']);
+                    }
+                }
+        $max_date_select = array_unique($max_date_select);
+
+//        array_push(
+//            $max_date_select,
+//            array_unique( array_column($each_check_shedule_info, 'date_training')) )  ;
 
 
         return view('privacy', [
-            'check_shedule_id'=>$check_shedule_id,
+            'check_shedule_id' => $check_shedule_id,
             'max_date_select' => $max_date_select,
             'each_check_shedule_info' => $each_check_shedule_info,
         ]);
@@ -132,7 +158,7 @@ class SingupController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -143,7 +169,7 @@ class SingupController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -154,8 +180,8 @@ class SingupController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -166,7 +192,7 @@ class SingupController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- localhost/resources/views/singup/success_singup_list.blade.php
+++ localhost/resources/views/singup/success_singup_list.blade.php
@@ -4,43 +4,135 @@
         <div class="container">
             <div class="row  mb-5">
                 <div class="col-md-12">
-                    <h2 class="site-section-heading text-center">Вы успешно запсаны на занятия:</h2>
+                    <h2 class="site-section-heading text-center">Добро пожаловать в Ваш личный кабинет:)</h2>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12 block-14 nav-direction-white">
+                <div class="col-md-12">
+
+                    {{--------------------------------------------------}}
+
+                    {{--{{ print_r(Auth::user()->getAuthIdentifier()) }}--}}
+
+                    @if(isset($max_date_select) && !empty($max_date_select))
+                        @if(isset($each_check_shedule_info) && !empty($each_check_shedule_info))
 
 
-                    {{--{{ print_r($_GET) }}--}}
-                    {{--<br />--}}
-                    {{--{{ print_r($each_check_shedule_info) }}--}}
-                    {{--{{ var_dump($check_shedule_id) }}--}}
+                            <h2 class="site-section-heading text-center">Вы успешно запсаны на занятия:</h2>
 
-                    {{--@foreach($check_shedule_id as $k=>$singup)--}}
 
-                        @foreach($each_check_shedule_info as $kk=>$singup)
+                            @auth
+                                {{--<form method='POST' action="{{ action('shedule\SheduleController@store') }}"--}}
+                                      {{--class="row">--}}
+                                    <form method='POST' action="{{ action('SingupController@update') }}" class="row">
+                                        @csrf
+                                        <?php $format_date = '';?>
+                                        @endauth
 
-                        @foreach($singup as $k=>$v)
 
-                            {{--{{ print_r($singup[$k]['shedule_id']) }}--}}
-                                <ul>
-                            <li>id  {{ $singup[$k]['shedule_id'] }}</li>
-                            <li>id  {{ $singup[$k]['date_training'] }}</li>
-                            <li>id  {{ $singup[$k]['start_training'] }} - {{ $singup[$k]['stop_training'] }}</li>
-                            <li>id  {{ $singup[$k]['section_title'] }}</li>
-                            <li>id  {{ $singup[$k]['trainer_name'] }}</li>
-                            <li>id  {{ $singup[$k]['gym_id'] }}</li>
-                                </ul>
-                        @endforeach
+                                        {{--Для каждой уникальной даты из расписания создаем таблицу--}}
+                                        @foreach($max_date_select as $k =>$date)
+                                            {{--{{ print_r($max_date_select) }}--}}
+                                            <?php
+                                            //  $format_date =  date_format(date_create($date), 'd-m-Y');
+                                            setlocale(LC_TIME, 'ru_RU.utf8');
+                                            $format_date = strftime("%a %e %B %G", strtotime($date));
+                                            ?>
 
-                        @endforeach
-
-                    {{--@endforeach--}}
+                                            <span
+                                                class="text-black">Запись на  &bullet; <strong>{{ $format_date }}</strong></span>
 
 
 
+                                            <table class="table table-striped">
+                                                <thead class="text-black thead-dark">
+                                                <tr>
+                                                    <th scope="col">Дата</th>
+                                                    <th scope="col">Время</th>
+                                                    <th scope="col">Тренер</th>
+                                                    <th scope="col">Секция</th>
+                                                    <th scope="col">№ зала</th>
+                                                    @auth
+                                                        <th scope="col">Отменить</th> @endauth
+                                                </tr>
+                                                </thead>
+                                                <tbody>
 
 
+                                                {{--Для каждой записи (неуникальной) из расписания--}}
+                                                @foreach($each_check_shedule_info as $kk=>$singup)
+                                                    @foreach($singup as $k=>$v)
+
+
+
+                                                    {{--Для каждой уникальной даты из расписания выводим все записи для этой даты--}}
+                                                    @if((strtotime($singup[$k]['date_training']) === strtotime($date)))
+
+                                                        <?php
+                                                        $section = $singup[$k]['section_title'];
+
+                                                        switch ($singup[$k]['section_title']) {
+                                                            case "morning_programs":
+                                                                $section = "Утренние программы";
+                                                                break;
+                                                            case "body_building":
+                                                                $section = "Боди билдинг";
+                                                                break;
+                                                            case "stretching":
+                                                                $section = "Стретчинг";
+                                                                break;
+                                                            case "fitness":
+                                                                $section = "Фитнес";
+                                                                break;
+                                                            case "yoga":
+                                                                $section = "Йога";
+                                                                break;
+                                                            case "child_programs":
+                                                                $section = "Детсткие программы";
+                                                                break;
+                                                        }
+                                                        ?>
+
+                                                        <tr>
+                                                            <td>{{ date_format(date_create($singup[$k]['date_training']), 'd-m-Y') }} -
+                                                                {{ $singup[$k]['shedule_id'] }}</td>
+                                                            <td>{{ date_format(date_create($singup[$k]['start_training']), 'H:i') }}
+                                                                - {{ date_format(date_create($singup[$k]['stop_training']), 'H:i') }}</td>
+                                                            <td>{{ $singup[$k]['trainer_name'] }}</td>
+                                                            <td>{{ $section }}</td>
+                                                            <td>{{ $singup[$k]['gym_id'] }}</td>
+                                                            @auth
+                                                                <td>
+                                                                    <label for="check_shedule_id">
+                                                                        <input id="check_shedule_id" type="checkbox"
+                                                                               name="check_shedule_id[]"
+                                                                               value="{{ $singup[$k]['shedule_id'] }}">
+                                                                    </label>
+                                                                </td>
+                                                            @endauth
+                                                        </tr>
+                                                    @endif
+                                                @endforeach
+                                                @endforeach
+
+                                                </tbody>
+                                            </table>
+                                        @endforeach
+
+
+                           @auth
+                               @if($format_date!=='')
+                                  <div class="col text-md-right  mb-3">
+                                         <input type="submit" class="btn btn-primary rounded text-white px-4" value="Отменить">
+                                   </div>
+                               @endif
+                                    </form>
+                            @endauth
+
+
+                        @endif
+                    @endif
+                    {{--------------------------------------------------}}
 
 
                 </div>
@@ -50,3 +142,25 @@
 
 
 @endif
+
+
+
+
+
+{{--@foreach($each_check_shedule_info as $kk=>$singup)--}}
+
+{{--@foreach($singup as $k=>$v)--}}
+
+{{--<ul>--}}
+{{--<li>id {{ $singup[$k]['shedule_id'] }}</li>--}}
+{{--<li>id {{ $singup[$k]['date_training'] }}</li>--}}
+{{--<li>id {{ $singup[$k]['start_training'] }} - {{ $singup[$k]['stop_training'] }}</li>--}}
+{{--<li>id {{ $singup[$k]['section_title'] }}</li>--}}
+{{--<li>id {{ $singup[$k]['trainer_name'] }}</li>--}}
+{{--<li>id {{ $singup[$k]['gym_id'] }}</li>--}}
+{{--</ul>--}}
+{{--@endforeach--}}
+
+{{--@endforeach--}}
+
+
